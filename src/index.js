cf475cedb8a44aa647154422ab839979b244fb32
Shamil <Shamilllaa@mail.ru>
2021-03-13 13:10:10 +0700

    11

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { compose, createStore, applyMiddleware } from 'redux';
+// import { compose, createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import App from './App';
 import { rootReducer } from './redux/rootReducer';
 import './index.css';
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-const store = createStore(rootReducer, compose(
-	// applyMiddleware(thunk),
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+// const store = createStore(rootReducer, compose(
+// 	applyMiddleware(thunk),
+// 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// ));
 
 const app = (
 	<Provider store={store}>
